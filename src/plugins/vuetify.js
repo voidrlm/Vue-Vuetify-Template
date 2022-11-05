6696825410eba6b13ad91b08c7815c9f7078e593
cHaOs <varjun609@gmail.com>
2022-11-06 02:39:06 +0530

    Added dark and light theme configurations

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,25 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#ffcc50",
+        secondary: "#b0bec5",
+        error: "#ff8888",
+        success: "#45a5ae",
+        accent: "#8c9eff",
+      },
+      dark: {
+        primary: "#333d9e",
+        secondary: "#b0bec5",
+        error: "#ff8888",
+        success: "#45a5ae",
+        accent: "#8c9eff",
+      },
+    },
+  },
 });
